15b7b1cfa42c060704650fbac3275938b8de748b
keigo06 <go.kbirdblue411@gmail.com>
2025-06-01 18:12:01 +0900

    feat: add headless mode and add abstacle_sub

---
+++ aichallenge/run_simulator_headless.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+AWSIM_DIRECTORY=/aichallenge/simulator/AWSIM_GPU
+
+# ヘッドレスモード用のオプション
+opts=("-batchmode" "-nographics")
+
+# shellcheck disable=SC1091
+source /aichallenge/workspace/install/setup.bash
+sudo ip link set multicast on lo
+sudo sysctl -w net.core.rmem_max=2147483647 >/dev/null
+$AWSIM_DIRECTORY/AWSIM.x86_64 "${opts[@]}" 
--- aichallenge/workspace/src/aichallenge_submit/path_to_trajectory/include/path_to_trajectory/path_to_trajectory.hpp
+++ aichallenge/workspace/src/aichallenge_submit/path_to_trajectory/include/path_to_trajectory/path_to_trajectory.hpp
@@ -18,20 +18,27 @@
 #include "autoware_auto_planning_msgs/msg/trajectory.hpp"
 #include "autoware_auto_planning_msgs/msg/trajectory_point.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 class PathToTrajectory : public rclcpp::Node {
  public:
   using PathWithLaneId = autoware_auto_planning_msgs::msg::PathWithLaneId;
   using Trajectory = autoware_auto_planning_msgs::msg::Trajectory;
   using TrajectoryPoint = autoware_auto_planning_msgs::msg::TrajectoryPoint;
+  using MarkerArray = visualization_msgs::msg::MarkerArray;
 
  public:
   PathToTrajectory();
 
  private:
-  void callback(const PathWithLaneId::SharedPtr msg);
-  rclcpp::Subscription<PathWithLaneId>::SharedPtr sub_;
-  rclcpp::Publisher<Trajectory>::SharedPtr pub_;
+  void pathCallback(const PathWithLaneId::SharedPtr msg);
+
+  rclcpp::Subscription<PathWithLaneId>::SharedPtr path_sub_;
+  rclcpp::Subscription<MarkerArray>::SharedPtr objects_sub_;
+  rclcpp::Publisher<Trajectory>::SharedPtr trajectory_pub_;
+
+  // 障害物の位置情報を保持するメンバ変数
+  MarkerArray::SharedPtr objects_;
 };
 
 #endif  // PATH_TO_TRAJECTORY__PATH_TO_TRAJECTORY_HPP_
